69f61a9e76fdc0e587f3c24b24136ffb84895e15
John Doe <johndoe@email.com>
2024-11-24 17:57:02 +0000

    commit

--- client/src/sections/Features.jsx
+++ client/src/sections/Features.jsx
@@ -18,16 +18,18 @@ const Features = () => {
                   <div className="-ml-3 mb-12 flex flex-col items-center justify-center">
                     <div className="w-0.5 h-16 bg-s3" />
                     <img
-                    width='112'
-                    height='112'
+                      width="112"
+                      height="112"
                       src={icon}
-                      className=" object-contain"
+                      // className=" object-contain"
                       alt={title}
                     />
                   </div>
                 </div>
                 <p className="caption mb-5 max-md:mb-6">{caption}</p>
-                <h2 className="max-w-400 mb-7 h3 text-p4 max-md:mb-6 max-md:h5">{title}</h2>
+                <h2 className="max-w-400 mb-7 h3 text-p4 max-md:mb-6 max-md:h5">
+                  {title}
+                </h2>
                 <p className="mb-11 body-1 max-md:mb-8 max-md:body-3">{text}</p>
                 <Button icon={button.icon}>{button.title}</Button>
               </div>
@@ -41,11 +43,13 @@ const Features = () => {
 
                   <div className="flex items-center justify-center mx-auto mb-3 border-2 border-s2 rounded-full hover:border-s4 transition-all duration-500 shadow-500 size-20">
                     <img
-                    // width='40'
-                    // height='40'
+                      width='40'
+                      height='40'
                       src={icon}
                       alt={title}
-                      className="size-17/20 object-contain z-20"
+                      // className="size-17/20 object-contain z-20"
+                      className="z-20"
+
                     />
                   </div>
 
